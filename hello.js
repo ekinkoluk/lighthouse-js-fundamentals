15c409681172b090f916f0ace64f4e539d033eff
Ekin Koluk <ekinkoluk@gmail.com>
2021-10-11 02:10:39 +0000

    function with return

--- hello.js
+++ hello.js
@@ -1,5 +1,14 @@
-const sayHello  = function () {
-  console.log("Hello, world");
+const sayHello  = function (name) {
+  console.log("Hello", + name);
 }
 
 sayHello();
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
