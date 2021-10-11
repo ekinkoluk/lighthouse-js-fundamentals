6c7cf556034144ef5efba61d6101f3135a7d9cf6
Ekin Koluk <ekinkoluk@gmail.com>
2021-10-11 02:19:25 +0000

    determine whether a number is odd or not

---
+++ odd.js
@@ -0,0 +1,20 @@
+function isOdd(number){
+  if(number === 0){
+        return false;
+  }
+
+  if (number === 1){
+        return true;
+  }
+ if(number % 2 === 0){
+    return false;
+
+  }
+
+  else{
+    return true;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
